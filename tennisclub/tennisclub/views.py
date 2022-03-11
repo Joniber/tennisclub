7282ce857888829d4f41bb0e062b2425b323b1af
Joniber <jonathanberwind02@gmail.com>
2022-03-11 14:15:27 +0100

    init

---
+++ tennisclub/tennisclub/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
+def index_view(request):
+    return render(request, 'index.html')
